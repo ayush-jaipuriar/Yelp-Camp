0ae55f49cf15f6e784dce59ad02fb955c39aebdf
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2020-01-23 23:39:15 +0530

    Added the landing page and the campground page

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var app = express();
 
 var campgrounds = [
     {
-        name: ""
+        name: " Camp Exotica", image: "https://www.holidify.com/images/cmsuploads/compressed/tent-1208201_1920_20190212172038.jpg"
+    },
+    {
+        name: " Camp Exotica", image: "https://www.holidify.com/images/cmsuploads/compressed/tent-1208201_1920_20190212172038.jpg"
     }
-]
+];
 
 app.set("view engine", "ejs");
 app.get("/", function (req, res) {
@@ -13,7 +16,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-    res.render("campground");
+    res.render("campground", {campgrounds:campgrounds});
 })
 
 app.listen(3000, function () {
